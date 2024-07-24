7f1d4206f68703ff95dcd4ca40650cea68cba09c
Dhanika29 <dhanikaboyilla29@gmail.com>
2024-07-24 19:59:23 +0530

    modified

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { useState } from "react";
 export default function Login() {
+  const[user, setUser]=useState()
+  const[password,setPassword]=useState()
+  const validateuser =() =>{
+   if((user === "john@gmail.com" && password === "1234"))
+   {
+    console.log("validuser");
+    
+   }
+    
+  }
   return (
     <div>
       <p>
-        <input type="text" placeholder="Username"></input>
+        <input type="text" onChange={}placeholder="Username"></input>
       </p>
       <p>
         <input type="password" placeholder="Password"></input>
       </p>
-      <button>Login</button>
+      <button onClick={validateuser}>Login</button>
     </div>
   );
 }
