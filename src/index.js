6414dbe2958691dbc8937d2fbe3d95c7027e1ac5
Dhanika29 <dhanikaboyilla29@gmail.com>
2024-07-24 20:39:19 +0530

    App31

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App32 from "./App32";
+import App31  from "./App31";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App32 />);
+root.render(<App31 />);
